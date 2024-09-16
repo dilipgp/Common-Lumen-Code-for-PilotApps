63abc6cfc62ba68015b5b25e0a74396c0c983bf4
Dilip Singh <dilip.singh@coxautoinc.com>
2024-09-16 18:24:48 +0530

    Updated Storage Key for state file

--- backend.tf
+++ backend.tf
@@ -3,7 +3,8 @@ terraform {
     resource_group_name  = "Github"
     storage_account_name = "devopslumen"
     container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
+    key                  = "terraform.tfstate"
 
   }
 }
+
