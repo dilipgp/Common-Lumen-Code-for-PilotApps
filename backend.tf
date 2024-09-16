905d1eb62ed2fe247694be4a8218ea0aacadd86d
dilipgp <45794342+dilipgp@users.noreply.github.com>
2024-09-16 17:51:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "Github"
     storage_account_name = "devopslumen"
     container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
+    key                  = "terraform.tfstate"
 
   }
 }
