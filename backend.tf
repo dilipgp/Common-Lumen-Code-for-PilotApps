f405979ce490e24747e81d970ab538b4df2cb65f
Dilip Singh <dilip.singh@coxautoinc.com>
2024-09-17 13:17:01 +0530

    Updated Service Pricipla with MSDN tenent

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Github"
-    storage_account_name = "devopslumen"
+    storage_account_name = "devopslumen01"
     container_name       = "tfstatefile"
     key                  = "terraform.tfstate"
 
