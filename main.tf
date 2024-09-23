b93d35d6c95e7bbdd152a420ac7ef1779ffa0753
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-23 16:01:10 +0530

    test

--- main.tf
+++ main.tf
@@ -78,29 +78,33 @@ module "avm-res-storage-storageaccount" {
 #   virtual_desktop_host_pool_type                = var.virtual_desktop_host_pool_type
 # }
 
-module "avd" {
-  source = "Azure/avm-ptn-avd-lza-managementplane/azurerm"
-  enable_telemetry                                   = var.enable_telemetry
-  location                                           = azurerm_resource_group.this.location
-  resource_group_name                                = azurerm_resource_group.this.name
-  virtual_desktop_workspace_name                     = var.virtual_desktop_workspace_name
-  virtual_desktop_scaling_plan_time_zone             = var.virtual_desktop_scaling_plan_time_zone
-  virtual_desktop_scaling_plan_name                  = var.virtual_desktop_scaling_plan_name
-  virtual_desktop_host_pool_type                     = var.virtual_desktop_host_pool_type
-  virtual_desktop_host_pool_load_balancer_type       = var.virtual_desktop_host_pool_load_balancer_type
-  virtual_desktop_host_pool_name                     = var.virtual_desktop_host_pool_name
+module "avm-res-desktopvirtualization-hostpool" {
+  source  = "Azure/avm-res-desktopvirtualization-hostpool/azurerm"
+  version = "0.2.1"
+  resource_group_name = azurerm_resource_group.this.name
+  virtual_desktop_host_pool_load_balancer_type = var.virtual_desktop_host_pool_load_balancer_type
+  virtual_desktop_host_pool_location = var.location
+  virtual_desktop_host_pool_name = var.virtual_desktop_host_pool_name
+  virtual_desktop_host_pool_resource_group_name = var.resource_group_name
+  virtual_desktop_host_pool_type = var.virtual_desktop_host_pool_type
   virtual_desktop_host_pool_maximum_sessions_allowed = var.virtual_desktop_host_pool_maximum_sessions_allowed
   virtual_desktop_host_pool_start_vm_on_connect      = var.virtual_desktop_host_pool_start_vm_on_connect
-  virtual_desktop_application_group_type             = var.virtual_desktop_application_group_type
-  virtual_desktop_application_group_name             = var.virtual_desktop_application_group_name
-  virtual_desktop_host_pool_friendly_name            = var.virtual_desktop_host_pool_friendly_name
-  log_analytics_workspace_name                       = module.naming.log_analytics_workspace.name_unique
+  virtual_desktop_host_pool_vm_template = {
+    type = "Gallery"
+    gallery_image_reference = {
+      offer     = "office-365"
+      publisher = "microsoftwindowsdesktop"
+      sku       = "22h2-evd-o365pp"
+      version   = "latest"
+    }
+    osDisktype = "PremiumLRS"
+  }
 }
 
 module "avm-res-desktopvirtualization-applicationgroup" {
   source                                                = "Azure/avm-res-desktopvirtualization-applicationgroup/azurerm"
   version                                               = "0.1.5"
-  virtual_desktop_application_group_host_pool_id        = module.avd.resource_id
+  virtual_desktop_application_group_host_pool_id        = module.avm-res-desktopvirtualization-hostpool.resource_id
   virtual_desktop_application_group_location            = var.location
   virtual_desktop_application_group_name                = var.virtual_desktop_application_group_name
   virtual_desktop_application_group_resource_group_name = var.resource_group_name
