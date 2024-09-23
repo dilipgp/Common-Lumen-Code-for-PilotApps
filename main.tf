61ee2c29d5c146a4a03fbcf548e52616ecb7199a
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-23 17:26:52 +0530

    test

--- main.tf
+++ main.tf
@@ -31,17 +31,6 @@ module "naming" {
 }
 
 # module "avm-res-keyvault-vault_example_default" {
-#   source  = "Azure/avm-res-keyvault-vault/azurerm"
-#   version = "0.9.1"
-#   location            = var.location
-#   resource_group_name = azurerm_resource_group.this.name
-#   name                = var.keyvault_name
-#   enable_telemetry    = var.enable_telemetry
-#   tenant_id           = var.tenant_id
-#   contacts            = var.contacts
-# }
-
-# module "avm-res-keyvault-vault_example_default" {
 #   source              = "Azure/avm-res-keyvault-vault/azurerm"
 #   version             = "0.9.1"
 #   location            = var.location
@@ -51,25 +40,6 @@ module "naming" {
 #   tenant_id           = var.tenant_id
 # }
 
-
-module "avm-res-storage-storageaccount" {
-  source  = "Azure/avm-res-storage-storageaccount/azurerm"
-  version = "0.2.7"
-  location = var.location
-  name = var.storage_accont_name
-  resource_group_name = azurerm_resource_group.this.name
-}
-
-# module "avm-res-desktopvirtualization-hostpool" {
-#   source                                        = "Azure/avm-res-desktopvirtualization-hostpool/azurerm"
-#   version                                       = "0.2.1"
-#   resource_group_name                           = var.resource_group_name
-#   virtual_desktop_host_pool_load_balancer_type  = var.virtual_desktop_host_pool_load_balancer_type
-#   virtual_desktop_host_pool_location            = var.location
-#   virtual_desktop_host_pool_name                = var.virtual_desktop_host_pool_name
-#   virtual_desktop_host_pool_resource_group_name = var.resource_group_name
-#   virtual_desktop_host_pool_type                = var.virtual_desktop_host_pool_type
-# }
 
 module "avm-res-desktopvirtualization-hostpool" {
   source  = "Azure/avm-res-desktopvirtualization-hostpool/azurerm"
@@ -111,24 +81,3 @@ module "avm-res-desktopvirtualization-workspace" {
   virtual_desktop_workspace_name                = var.virtual_desktop_workspace_name
   virtual_desktop_workspace_resource_group_name = azurerm_resource_group.this.name
 }
-
-
-# module "avd" {
-#   source = "./modules/avd"
-#   # source             = "Azure/avm-ptn-avd-lza-managementplane/azurerm"
-#   enable_telemetry                                   = var.enable_telemetry
-#   location                                           = azurerm_resource_group.this.location
-#   resource_group_name                                = azurerm_resource_group.this.name
-#   virtual_desktop_workspace_name                     = var.virtual_desktop_workspace_name
-#   virtual_desktop_scaling_plan_time_zone             = var.virtual_desktop_scaling_plan_time_zone
-#   virtual_desktop_scaling_plan_name                  = var.virtual_desktop_scaling_plan_name
-#   virtual_desktop_host_pool_type                     = var.virtual_desktop_host_pool_type
-#   virtual_desktop_host_pool_load_balancer_type       = var.virtual_desktop_host_pool_load_balancer_type
-#   virtual_desktop_host_pool_name                     = var.virtual_desktop_host_pool_name
-#   virtual_desktop_host_pool_maximum_sessions_allowed = var.virtual_desktop_host_pool_maximum_sessions_allowed
-#   virtual_desktop_host_pool_start_vm_on_connect      = var.virtual_desktop_host_pool_start_vm_on_connect
-#   virtual_desktop_application_group_type             = var.virtual_desktop_application_group_type
-#   virtual_desktop_application_group_name             = var.virtual_desktop_application_group_name
-#   virtual_desktop_host_pool_friendly_name            = var.virtual_desktop_host_pool_friendly_name
-#   log_analytics_workspace_name                       = module.naming.log_analytics_workspace.name_unique
-# }
