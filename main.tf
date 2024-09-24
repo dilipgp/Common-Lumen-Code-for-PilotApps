a2d92df90552cadb278faf1466cbb8e1f7f16354
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-24 18:40:35 +0530

    test

--- main.tf
+++ main.tf
@@ -149,22 +149,22 @@ module "avm-res-operationalinsights-workspace" {
 module "storage_account" {
   source  = "Azure/avm-res-storage-storageaccount/azurerm"
   version = "0.2.7"
-
   name                = "lumentestavd"
-  location           = "eastus2"
+  location            = "eastus2"
   resource_group_name = azurerm_resource_group.this.name
-  shares = {
-    share1 = {
-      name = "share1"
-      quota = 1024
-    }
-  }
+}
 
-  containers = {
-    container1 = {
-      name = "container1"
-      public_access = "Container"
-    }
-  }
-  
+resource "azurerm_storage_share" "example" {
+  name                 = "sharename"
+  storage_account_name = module.storage_account.name
+  quota                = 50
+  # acl {
+  #   id = "MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI"
+
+  #   access_policy {
+  #     permissions = "rwdl"
+  #     start       = "2019-07-02T09:38:21.0000000Z"
+  #     expiry      = "2019-07-02T10:38:21.0000000Z"
+  #   }
+  # }
 }
