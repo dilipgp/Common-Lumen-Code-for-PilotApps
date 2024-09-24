a8ec8133c7a88070a63f4894554c00d2f49691e9
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-25 00:00:41 +0530

    test

--- main.tf
+++ main.tf
@@ -156,7 +156,7 @@ resource "azurerm_virtual_network" "example" {
 resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.this.name
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
