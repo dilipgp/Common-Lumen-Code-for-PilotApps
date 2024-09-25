b117ae306df8d533f385375469d9e3b594608546
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-25 15:22:11 +0530

    test

--- main.tf
+++ main.tf
@@ -22,18 +22,84 @@ module "naming" {
 # }
 
 module "avm-res-storage-storageaccount" {
-  source  = "Azure/avm-res-storage-storageaccount/azurerm"
-  version = "0.2.7"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.this.name
-  name                = "salumentestmsft"
-  shared_access_key_enabled = true
+  source                                  = "Azure/avm-res-storage-storageaccount/azurerm"
+  version                                 = "0.2.7"
+  name                                    = "satestlumenmsft"
+  resource_group_name                     = azurerm_resource_group.this.name
+  location                                = var.location
+  # account_tier                            = var.account_tier
+  # account_replication_type                = var.account_replication_type
+  # account_kind                            = var.account_kind
+  # access_tier                             = var.access_tier
+  # tags                                    = local.tags
+  # public_network_access_enabled           = var.public_network_access_enabled
+  # allow_nested_items_to_be_public         = var.sa_allow_nested_items_to_be_public
+  # infrastructure_encryption_enabled       = var.sa_infrastructure_encryption_enabled
+  # shared_access_key_enabled               = var.shared_access_key_enabled
+  # enable_telemetry                        = var.enable_telemetry
+  shares                                  = {
+  share0 = {
+    name        = "fileshare-1"
+    quota       = 10
+    access_tier = "Hot"
+    metadata = {
+      key1 = "value1"
+      key2 = "value2"
+    }
+    signed_identifiers = [
+      {
+        id = "1"
+        access_policy = {
+          expiry_time = "2025-01-01T00:00:00Z"
+          permission  = "r"
+          start_time  = "2024-01-01T00:00:00Z"
+        }
+      }
+    ]
+  }
 }
-
-resource "azurerm_storage_share" "fileshare" {
-  name                 = "fs-fslx-01"
-  storage_account_name = module.avm-res-storage-storageaccount.name
-  quota                = 300
+  # tables                                  = var.tables
+  # queues                                  = var.queues
+  # containers                              = var.containers
+  # share_properties                        = var.share_properties
+  # large_file_share_enabled                = var.large_file_share_enabled
+  # azure_files_authentication              = var.azure_files_authentication
+  # immutability_policy                     = var.immutability_policy
+  # is_hns_enabled                          = var.is_hns_enabled
+  # blob_properties                         = var.blob_properties
+  # table_encryption_key_type               = var.table_encryption_key_type
+  # queue_encryption_key_type               = var.queue_encryption_key_type
+  # queue_properties                        = var.queue_properties
+  # private_endpoints                       = var.private_endpoints
+  # storage_management_policy_rule          = var.storage_management_policy_rule
+  # storage_management_policy_timeouts      = var.storage_management_policy_timeouts
+  # diagnostic_settings_storage_account     = var.diagnostic_settings_storage_account
+  # diagnostic_settings_blob                = var.diagnostic_settings_blob
+  # diagnostic_settings_queue               = var.diagnostic_settings_queue
+  # diagnostic_settings_table               = var.diagnostic_settings_table
+  # diagnostic_settings_file                = var.diagnostic_settings_file
+  # storage_data_lake_gen2_filesystem       = var.storage_data_lake_gen2_filesystem
+  # customer_managed_key                    = var.customer_managed_key
+  # lock                                    = var.lock
+  # managed_identities                      = var.managed_identities
+  # private_endpoints_manage_dns_zone_group = var.private_endpoints_manage_dns_zone_group
+  # role_assignments                        = var.role_assignments
+  # allowed_copy_scope                      = var.allowed_copy_scope
+  # cross_tenant_replication_enabled        = var.cross_tenant_replication_enabled
+  # custom_domain                           = var.custom_domain
+  # default_to_oauth_authentication         = var.default_to_oauth_authentication
+  # edge_zone                               = var.edge_zone
+  # https_traffic_only_enabled              = var.https_traffic_only_enabled
+  # local_user                              = var.local_user
+  # min_tls_version                         = var.min_tls_version
+  # network_rules                           = var.network_rules
+  # nfsv3_enabled                           = var.nfsv3_enabled
+  # routing                                 = var.routing
+  # sas_policy                              = var.sas_policy
+  # sftp_enabled                            = var.sftp_enabled
+  # static_website                          = var.static_website
+  # timeouts                                = var.timeouts
+ 
 }
 
 
