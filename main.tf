88e5d27fb69672e3e2bc6118e0a846678e042f67
Yashwi Kumari <yashwikuamri23@gmail.com>
2024-09-27 17:41:35 +0530

    updated code

--- main.tf
+++ main.tf
@@ -323,7 +323,7 @@ module "avm-res-desktopvirtualization-workspace" {
   subresource_names                             = ["feed"]
   private_endpoints = {
     primary = {
-      private_dns_zone_resource_ids = module.avm-res-network-privatednszone.this.id
+      domain_name = var.domain_name
       subnet_resource_id            = azurerm_subnet.example.id
     }
   }
