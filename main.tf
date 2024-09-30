adb3855d6b7e6f9be9d207ca1740773a2c77de98
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-30 17:09:23 +0530

    test

--- main.tf
+++ main.tf
@@ -386,19 +386,19 @@ resource "azurerm_storage_share" "example" {
 # }
 
 
-# resource "azurerm_virtual_network" "example" {
-#   name                = "example-network"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.this.location
-#   resource_group_name = azurerm_resource_group.this.name
-# }
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
 
-# resource "azurerm_subnet" "example" {
-#   name                 = "internal"
-#   resource_group_name  = azurerm_resource_group.this.name
-#   virtual_network_name = azurerm_virtual_network.example.name
-#   address_prefixes     = ["10.0.2.0/24"]
-# }
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
 # module "avm-res-compute-virtualmachine" {
 #   source  = "Azure/avm-res-compute-virtualmachine/azurerm"
