78c73456c16d9c88c4e75a06e8e57d3ccd8ad22a
Yashwi Kumari <yashwikuamri23@gmail.com>
2024-10-03 12:34:55 +0530

    updated secrets

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "azurerm_key_vault_secret" "secrets" {
   for_each        = { for secret in var.secrets : secret.name => secret }
   key_vault_id    = module.avm-res-keyvault-vault.resource_id
   name            = each.value.name
-  value           = each.value.value
+  value           = random_password.pass
   content_type    = each.value.content_type
   not_before_date = each.value.not_before_date
   expiration_date = each.value.expiration_date
