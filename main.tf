b42c16724eebb4d30d3d368f7b43df11914e10f2
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-10-04 14:50:48 +0530

    test

--- main.tf
+++ main.tf
@@ -340,7 +340,7 @@ module "avm-res-operationalinsights-workspace" {
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.10.0.0/16"]
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
@@ -349,7 +349,7 @@ resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.10.2.0/24"]
 }
 
 module "avm-res-compute-virtualmachine" {
